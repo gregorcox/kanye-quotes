dcc3efc0576267b2cb334e7b0fcb5243670cbcac
Gregor Cox <gregor@fxdigital.uk>
2019-09-29 19:12:38 +0100

    change css class based on album state

--- src/containers/QuoteGenerator.js
+++ src/containers/QuoteGenerator.js
@@ -34,30 +34,33 @@ class QuoteGenerator extends React.Component {
     }
 
     render() {
+        const { album } = this.state;
+
         return(
             <div className="container">
 
-                <div>
+                <div className={`album-container album-container--${album}`}>
                     <label htmlFor="album-select">Choose an album:</label>
 
                     <select id="album-select" onChange={this.handleAlbumChage}>
-                        <option value="">--Please choose an option--</option>
+                        <option value="">--Please choose an album--</option>
                         <option value="college">The College Dropout</option>
                         <option value="registration">Late Registration</option>
                         <option value="graduation">Graduation</option>
-                        <option value="heatbreak">808s and Heartbreak</option>
+                        <option value="heartbreak">808s and Heartbreak</option>
                         <option value="fantasy">My Beautiful Dark Twisted Fantasy</option>
                         <option value="throne">Watch The Throne</option>
                         <option value="yeezus">Yeezus</option>
                         <option value="pablo">The Life of Pablo</option>
                         <option value="ye">Ye</option>
+                        <option value="ghosts">Ghosts</option>
                     </select>
                 </div>
 
                 <div className="quote-container">
-                    <p>{this.state.quote}</p>
+                    <p className="quote">{this.state.quote}</p>
 
-                    <button type="button" onClick={this.generateQuote}>Generate Quote</button>
+                    <button className="button" type="button" onClick={this.generateQuote}>Generate Quote</button>
                 </div>
 
             </div>
